4d28bc6076f58ccbeedf8535bddeb86bbc0fc849
kaylamuraoka <kmurs98@gmail.com>
2020-12-04 15:30:48 -1000

    added connection to Heroku db

--- web/index.php
+++ web/index.php
@@ -22,4 +22,22 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+// Create connection
+$conn = new mysqli($server, $username, $password, $db);
+// Check connection
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+}
+  echo "Connected successfully";
+
 $app->run();
+
+
