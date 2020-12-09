2e1cc01b17294224496c4efbb5553db3d03b4f9d
kaylamuraoka <kmurs98@gmail.com>
2020-12-08 16:25:13 -1000

    updated index.php

--- web/index.php
+++ web/index.php
@@ -35,6 +35,19 @@ if ($conn->connect_error) {
    die("Connection failed: " . $conn->connect_error);
 }
   echo "Connected successfully";
+  
+  $sql = "SELECT * FROM district";
+  $result = $conn->query($sql);
+  
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+  $conn->close();
 
 $app->run();
 
