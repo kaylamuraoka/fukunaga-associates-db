89667e5f95a4bbb23db41b19c036594333f6d147
kaylamuraoka <kmurs98@gmail.com>
2020-12-14 15:38:32 -1000

    added styling to table data

--- web/index.php
+++ web/index.php
@@ -36,17 +36,61 @@ if ($conn->connect_error) {
 }
   echo "Connected successfully";
   
-  $sql = "SELECT * FROM district";
+  $sql = "SELECT * FROM schools_view";
   $result = $conn->query($sql);
-  
+
+  echo '<table class="table">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">DOE Code</th>
+                <th scope="col">Name</th>
+                <th scope="col">Address</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Fax</th>
+                <th scope="col">Principal</th>
+                <th scope="col">Grade Range</th>
+                <th scope="col">Type</th>
+                <th scope="col">Website</th>
+                <th scope="col">Complex</th>
+                <th scope="col">Complex Area</th>
+                <th scope="col">District/th>
+                <th scope="col">Island/th>
+                <th scope="col">Charter/th>
+                <th scope="col">Esis Name/th>
+              </tr>
+            </thead>
+            <tbody>';
+         
   if ($result->num_rows > 0) {
-    echo "This is the District Table:";
     // output data of each row
     while($row = $result->fetch_assoc()) {
-      echo "id: " . $row["id"]. " - District: " . $row["name"]. "<br>";
+      echo '<tr>';
+      echo '<th scope="row">'.$row["doe_code"].'</th>';
+      echo '<td>'.$row["name"].'</td>';
+      echo '<td>'.$row["address"].'</td>';
+      echo '<td>'.$row["phone"].'</td>';
+      echo '<td>'.$row["fax"].'</td>';
+      echo '<td>'.$row["principal"].'</td>';
+      echo '<td>'.$row["grade_range"].'</td>';
+      echo '<td>'.$row["type"].'</td>';
+      echo '<td>'.$row["website"].'</td>';
+      echo '<td>'.$row["complex"].'</td>';
+      echo '<td>'.$row["complex_area"].'</td>';
+      echo '<td>'.$row["district"].'</td>';
+      echo '<td>'.$row["island"].'</td>';
+      echo '<td>'.$row["charter"].'</td>';
+      echo '<td>'.$row["esis_name"].'</td>';
+      echo '</tr>';
     }
+    echo '</tbody>
+        </table>'; 
   } else {
-    echo "0 results";
+    echo '<tr>
+            <td colspan="15">0 results found</td>
+
+          </tr>
+        </tbody>
+      </table>'; 
   }
 
   // $sql = "SELECT complex_area.id, complex_area.name, CONCAT(complex_area.address, " ", complex_area.city, ", HI ", complex_area.zip_code) AS address, complex_area.phone,     complex_area.fax, district.name AS district FROM complex_area LEFT JOIN district ON complex_area.district_id=district.id";
