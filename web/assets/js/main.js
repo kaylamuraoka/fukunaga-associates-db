aae543b3142e675c1846c5203c23a484160f1975
kaylamuraoka <kmurs98@gmail.com>
2020-12-22 18:25:02 -1000

    change the profile image

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function (e) {
+  let $uploadFile = $('#register .upload-profile-image input[type="file"]');
+
+  $uploadFile.change(function () {
+    readURL(this);
+  });
+
   $("#reg-form").submit(function (event) {
     let $password = $("#password");
     let $confirm = $("#confirm_pwd");
@@ -13,3 +19,15 @@ $(document).ready(function (e) {
     }
   });
 });
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      $("#register .upload-profile-image .img").attr("src", e.target.result);
+      $("#register .upload-profile-image .camera-icon").css((display, "none"));
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
