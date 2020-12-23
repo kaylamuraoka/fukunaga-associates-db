5ce4e74f4f2b467d3384cbff270945acf3cd5ff6
kaylamuraoka <kmurs98@gmail.com>
2020-12-23 13:07:01 -1000

    editing db credentials file

--- web/config/db.php
+++ web/config/db.php
@@ -2,21 +2,29 @@
 /* Database credentials. To connect to MySQL server we need to use this database configuation file */
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-define('_DBHOST',$url["host"]);
-define('_DBUSER', $url["user"]);  
-define('_DBPASSWD', $url["pass"]);  
-define('_DBNAME', substr($url["path"], 1));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 
-try{
-   /* Attempt to connect to MySQL database */
-   $conn = new mysqli(_DBHOST, _DBUSER, _DBPASSWD, _DBNAME);
+$conn = new mysqli($server, $username, $password, $db);
 
-   // encoded language
-   mysqli_set_charset($conn, 'utf8');
-
-} catch (Exception $ex) {
-   // if there if any problem with the connection
-   print "An Exception occurred. Message: ".$ex->getMessage();
-} catch (Error $e){
-   print "The system is busy please try again later.";
+/* Attempt to connect to MySQL database */
+if ($conn -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $conn -> connect_error;
+  exit();
 }
+
+// try{
+   
+//    $conn = new mysqli(_DBHOST, _DBUSER, _DBPASSWD, _DBNAME);
+
+//    // encoded language
+//    mysqli_set_charset($conn, 'utf8');
+
+// } catch (Exception $ex) {
+//    // if there if any problem with the connection
+//    print "An Exception occurred. Message: ".$ex->getMessage();
+// } catch (Error $e){
+//    print "The system is busy please try again later.";
+// }
