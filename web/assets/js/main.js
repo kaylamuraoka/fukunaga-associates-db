db11ee384d1a02baecfbbe7b8cbd8acadb800e3d
kaylamuraoka <kmurs98@gmail.com>
2020-12-22 17:54:32 -1000

    adding js to handle form submit

---
+++ web/assets/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function (e) {
+  $("#reg-form").submit(function (event) {
+    let $password = $("#password");
+    let $confirm = $("#confirm_pwd");
+    let $error = $("#confirm_error");
+    if ($password.val() === $confirm.val()) {
+      return true;
+    } else {
+      $error.text("Password does not match");
+      event.preventDefault();
+    }
+  });
+});
