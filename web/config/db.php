9aad52297778b64bd2f085c7de6b5aa692b5786d
kaylamuraoka <kmurs98@gmail.com>
2020-12-23 13:21:24 -1000

    text db connection

--- web/config/db.php
+++ web/config/db.php
@@ -13,6 +13,8 @@ $conn = new mysqli($server, $username, $password, $db);
 if ($conn -> connect_errno) {
   echo "Failed to connect to MySQL: " . $conn -> connect_error;
   exit();
+} else {
+  echo "Connected to database successfully.";
 }
 
 // try{
