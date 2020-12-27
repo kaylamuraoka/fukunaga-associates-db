1b89d361bc5733dd3aced1b3e299c9571c6ef110
kaylamuraoka <kmurs98@gmail.com>
2020-12-26 19:27:12 -1000

    adding styling

--- web/index.php
+++ web/index.php
@@ -17,17 +17,17 @@ if(isset($_SESSION['userID'])){
   <div class="container py-5">
     <div class="row">
       <div class="col-4 offset-4 shadow py-4"><div class="upload-profile-image d-flex justify-content-center pb-5">
-        <img class="img rounded-circle" style="width: 200px; height: 200px;" src="<?php echo isset($user['profileImg']) ? $user['profileImg']:'./assets/images/profile/default_avatar.png';?>" alt="profile">
-        <h4 class="py-3">
-          <?php
-          if(isset($user['firstName'])){
-            printf('%s %s', $user['firstName'], $user['lastName']);
-          }
-          ?>
-        </h4>
-
+        <div class="text-center">
+          <img class="img rounded-circle" style="width: 200px; height: 200px;" src="<?php echo isset($user['profileImg']) ? $user['profileImg']:'./assets/images/profile/default_avatar.png';?>" alt="profile">
+          <h4 class="py-3">
+            <?php
+            if(isset($user['firstName'])){
+              printf('%s %s', $user['firstName'], $user['lastName']);
+            }
+            ?>
+          </h4>
+        </div>
       </div>
-
     </div>
   </div>
 </section>
