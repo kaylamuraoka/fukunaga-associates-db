f9c28f7232639b815536f620be0498d87f95f6a4
kaylamuraoka <kmurs98@gmail.com>
2020-12-26 19:39:17 -1000

    adding styling

--- web/index.php
+++ web/index.php
@@ -16,16 +16,26 @@ if(isset($_SESSION['userID'])){
 <section id="main-site">
   <div class="container py-5">
     <div class="row">
-      <div class="col-4 offset-4 shadow py-4"><div class="upload-profile-image d-flex justify-content-center pb-5">
-        <div class="text-center">
-          <img class="img rounded-circle" style="width: 200px; height: 200px;" src="<?php echo isset($user['profileImg']) ? $user['profileImg']:'./assets/images/profile/default_avatar.png';?>" alt="profile">
-          <h4 class="py-3">
-            <?php
-            if(isset($user['firstName'])){
+      <div class="col-4 offset-4 shadow py-4">
+        <div class="upload-profile-image d-flex justify-content-center pb-5">
+
+          <div class="user-info px-3">
+            <ul class="font-ubuntu navbar-nav">
+              <li class="nav-link"><b>First Name: </b><span><?php echo isset($user['firstName']) ? $user['firstName']:'';?></span></li>
+              <li class="nav-link"><b>Last Name: </b><span><?php echo isset($user['lastName']) ? $user['lastName']:'';?></span></li>
+              <li class="nav-link"><b>Email: </b><span><?php echo isset($user['email']) ? $user['email']:'';?></span></li>
+            </ul>
+          </div>
+          <div class="text-center">
+            <img class="img rounded-circle" style="width: 200px; height: 200px;" src="<?php echo isset($user['profileImg']) ? $user['profileImg']:'./assets/images/profile/default_avatar.png';?>" alt="profile">
+            <h4 class="py-3">
+              <?php
+              if(isset($user['firstName'])){
               printf('%s %s', $user['firstName'], $user['lastName']);
-            }
-            ?>
-          </h4>
+              }
+              ?>
+            </h4>
+          </div>
         </div>
       </div>
     </div>
