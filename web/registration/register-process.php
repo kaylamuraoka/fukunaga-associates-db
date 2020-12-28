789127ac2536dc54160fce9daad8e5aad9d6c2f8
kaylamuraoka <kmurs98@gmail.com>
2020-12-28 11:38:50 -1000

    adding validation

--- web/registration/register-process.php
+++ web/registration/register-process.php
@@ -43,36 +43,59 @@ if (empty($error)) {
   $hashed_pass = password_hash($password, PASSWORD_DEFAULT);
   require("config/db.php");
 
-  // Make a query
-  $query = "INSERT INTO users (userID, firstName, lastName, email, password, profileImg, createdAt, updatedAt) VALUES ('',?,?,?,?,?,NOW(),NOW())";
+  // Check if email exists
+  $query = "SELECT email FROM users WHERE email=?";
 
   // Initialize a statement
   $stmt = mysqli_stmt_init($conn);
-
-  // Secure database from SQL injection
+  
   // Prepare SQL statement
   mysqli_stmt_prepare($stmt, $query);
 
   // Bind input text box values
-  mysqli_stmt_bind_param($stmt, 'sssss', $firstName, $lastName, $email, $hashed_pass, $profileImg);
+  mysqli_stmt_bind_param($stmt, 's', $email);
 
   // Execute prepared statement
   mysqli_stmt_execute($stmt);
+  $result = mysqli_stmt_get_result($stmt);
 
-  // Check if successfully inserted
-  if(mysqli_stmt_affected_rows($stmt)==1){
-    // start a new session
-    session_start();
-
-    // create session variable
-    $_SESSION['userID'] = mysqli_insert_id($conn);
-    
-    header('location: ../login.php');
-    exit();
-  } else{
-    print "Error while registration...!";
+  $row = mysqli_fetch_array($result);
+  if ($row > 0 ) {
+    // if $row is greater than 0, (means the email exists)
+    echo "Error: email already exists";
   }
+  else {
+    // Make a query
+    $query = "INSERT INTO users (userID, firstName, lastName, email, password, profileImg, createdAt, updatedAt) VALUES ('',?,?,?,?,?,NOW(),NOW())";
 
+    // Initialize a statement
+    $stmt = mysqli_stmt_init($conn);
+
+    // Secure database from SQL injection
+    // Prepare SQL statement
+    mysqli_stmt_prepare($stmt, $query);
+
+    // Bind input text box values
+    mysqli_stmt_bind_param($stmt, 'sssss', $firstName, $lastName, $email, $hashed_pass, $profileImg);
+
+    // Execute prepared statement
+    mysqli_stmt_execute($stmt);
+
+    // Check if successfully inserted
+    if(mysqli_stmt_affected_rows($stmt)==1){
+      // start a new session
+      session_start();
+
+      // create session variable
+      $_SESSION['userID'] = mysqli_insert_id($conn);
+    
+      header('location: ../login.php');
+      exit();
+    } else { 
+      // Error while registration
+      print "Error while registration...!";
+    }
+  }
 } else{
   print 'not validated';
 }
