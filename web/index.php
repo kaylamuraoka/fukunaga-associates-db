8707bc26791624ed84fa58d944108756302e7a4a
kaylamuraoka <kmurs98@gmail.com>
2020-12-29 10:56:27 -1000

    redirecting if not logged in

--- web/index.php
+++ web/index.php
@@ -1,14 +1,17 @@
 <?php
-
+// We need to use sessions, so you should always start sessions using the below code.
 session_start();
 include('layouts/header.php');
 include('registration/helper.php');
 
 $user = array();
-
+// If the user is not logged in redirect to the login page...
 if(isset($_SESSION['userID'])){
   require('config/db.php');
   $user = get_user_info($conn, $_SESSION['userID']);
+} else {
+  header('Location: index.html');
+	exit;
 }
 
 ?>
@@ -21,6 +24,7 @@ if(isset($_SESSION['userID'])){
           <div class="text-center">
             <img class="img rounded-circle" style="width: 200px; height: 200px;" src="<?php echo isset($user['profileImg']) ? $user['profileImg']:'./assets/images/profile/default_avatar.png';?>" alt="profile">
             <h4 class="py-3">
+              <p>Welcome back, <?=$user['firstName'], $user['lastName']?>!</p>
               <?php
               if(isset($user['firstName'])){
               printf('%s %s', $user['firstName'], $user['lastName']);
