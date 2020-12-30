67349e18958b9f644fd4e2d1478e49e89078f6ea
kaylamuraoka <kmurs98@gmail.com>
2020-12-29 17:16:29 -1000

    adding nav bar to index.php

--- web/index.php
+++ web/index.php
@@ -17,6 +17,43 @@ if(isset($_SESSION['userID'])){
 ?>
 
 <section id="main-site">
+  <!-- Navbar content -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+      aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Fukunaga & Associates</a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <!-- Home -->
+        <li class="nav-item active">
+          <a class="nav-link" href="#"><i class="fas fa-home"></i> Home<span class="sr-only">(current)</span></a>
+        </li>
+        <!-- Calendar -->
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fas fa-calendar-alt"></i> Calendar</a>
+        </li>
+        <!-- News & Updates -->
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fas fa-bell"></i> News & Updates</a>
+        </li>
+        <!-- Projects -->
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fas fa-folder-open"></i> Projects</a>
+        </li>
+        <!-- Tasks -->
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fas fa-tasks"></i> Tasks</a>
+        </li>
+        <!-- Settings -->
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fas fa-cog"></i> Settings</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
   <div class="container py-5">
     <div class="row">
       <div class="col-4 offset-4 shadow py-4">
