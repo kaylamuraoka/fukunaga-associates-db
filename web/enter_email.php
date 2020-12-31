7e86867952c21f52cc1630ca6cf7cdd7a8f6bf15
kaylamuraoka <kmurs98@gmail.com>
2020-12-30 18:13:45 -1000

    adding styliny to enter email page

--- web/enter_email.php
+++ web/enter_email.php
@@ -4,27 +4,33 @@
 ?>
 
 <!-- This Page renders the Email Entry Form to reset password content below -->
-<section id="main-site">
-  <h1>Hello</h1>
-  <form class="login-form" action="enter_email.php" method="post">
-    <h2 class="form-title">Reset password</h2>
-    <!-- form validation messages -->
-    <?php include('registration/errors.php'); ?>
-    
-    <!-- Email Input Fields -->
-    <label>Please enter your email address</label>
-    <div class="form-group">
-      <i class="fas fa-envelope"></i>
-      <input type="email" required name="email" id="email" class="form-control" placeholder="Email">
-    </div>
-    <div class="form-group">
-      <button type="submit" name="reset-password" class="login-btn">Submit</button>
-    </div>
-  </form>
+<section id="registration">
+  <div class="form-container">
+    <form action="enter_email.php" method="post">
+      <h1>Reset your password</h2>
+      <!-- Directions -->
+      <h5 class="text-muted pb-4">To reset your password, enter your email address and we'll send you an email with instructions.</h5>
+
+      <!-- form validation messages -->
+      <?php include('registration/errors.php'); ?>
+
+      <!-- Email Input Field -->
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+        </div>
+          <input type="email" required name="email" id="email" class="form-control" placeholder="Email Address">
+      </div>
+        <!-- Submit -->
+        <button type="submit" name="reset-password">Next</button>
+
+        <!-- Back to login -->
+       <a href="login.php">Go Back</a>
+    </form>  
+  </div>
 </section>
 
 <!-- Include page footer -->
 <?php
   include("layouts/footer.php");
 ?>
-
